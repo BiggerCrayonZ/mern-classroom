f4ddd8e77e064dc7f840903db3d5b2bcf9519260
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2020-02-09 14:36:20 -0600

    Database - local and rmeote switch composer

--- classroom-api/src/database.config.js
+++ classroom-api/src/database.config.js
@@ -1,5 +1,6 @@
 const mong = require("mongoose");
-const URI = `mongodb+srv://rreza:${process.env.mongodbPass}@rreza-qo8zt.mongodb.net/classroom?retryWrites=true&w=majority`;
+// const URI = `mongodb+srv://rreza:${process.env.mongodbPass}@rreza-qo8zt.mongodb.net/classroom?retryWrites=true&w=majority`;
+const URI = 'mongodb://localhost/mern-aula';
 
 mong.set('useFindAndModify', false);
 mong.set('useCreateIndex', true);
