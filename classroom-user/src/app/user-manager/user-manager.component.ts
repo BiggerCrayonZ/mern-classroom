2326687cfff038e665779b94a48d7ef3dc4df526
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2020-04-10 22:42:52 -0500

    user - evaluate blur input function

--- classroom-user/src/app/user-manager/user-manager.component.ts
+++ classroom-user/src/app/user-manager/user-manager.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { UserService } from '../user.service';
-import { Observable } from 'rxjs';
-import { User } from '../shared/model/user';
-import { Header } from '../shared/model/header';
-import { tap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { ModalService } from '../service/modal.service';
-import { Role } from '../shared/model/role';
-import { NgForm } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core'
+import { UserService } from '../user.service'
+import { Observable, fromEvent, Subscription } from 'rxjs'
+import { User } from '../shared/model/user'
+import { Header } from '../shared/model/header'
+import {
+  tap,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith
+} from 'rxjs/operators'
+import { ModalService } from '../service/modal.service'
+import { Role } from '../shared/model/role'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'manager0-user-manager',
@@ -14,58 +26,85 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./user-manager.component.sass']
 })
 export class UserManagerComponent implements OnInit {
+  loading = false
+  userIdExist = false
+  headers
+  users$: Observable<User[]>
+  roles$: Observable<Role[]>
 
-  loading = false;
-  headers;
-  users$: Observable<User[]>;
-  roles$: Observable<Role[]>;
+  private usernameSubscription: Subscription
 
-  @Input() token: string;
-  @Input() filter: string;
+  @Input() token: string
+  @Input() filter: string
 
-  constructor(
+  @ViewChild('username') searchUsername: any
+
+  constructor (
     private userService: UserService,
-    private modalService: ModalService,
-  ) { }
+    private modalService: ModalService
+  ) {}
 
-  ngOnInit(): void {
-    this.headers = this.userService.getTableHeader();
-    this.roles$ = this.userService.getUserRoles(this.token)
-      .pipe(
-        tap(_ => this.loading = true),
-        debounceTime(3000),
-        distinctUntilChanged(),
-        tap(_ => this.loading = false)
-      );
-    this.users$ = this.userService.getUsers(this.token, this.filter)
-      .pipe(
-        tap(_ => this.loading = true),
-        debounceTime(3000),
-        distinctUntilChanged(),
-        tap(_ => this.loading = false)
-      );
+  ngOnInit (): void {
+    this.headers = this.userService.getTableHeader()
+    this.roles$ = this.userService.getUserRoles(this.token).pipe(
+      tap(_ => (this.loading = true)),
+      debounceTime(3000),
+      distinctUntilChanged(),
+      tap(_ => (this.loading = false))
+    )
+    this.users$ = this.userService.getUsers(this.token, this.filter).pipe(
+      tap(_ => (this.loading = true)),
+      debounceTime(3000),
+      distinctUntilChanged(),
+      tap(_ => (this.loading = false))
+    )
   }
 
-  refresh(): void {
-    this.users$ = this.userService.getUsers(this.token, this.filter)
-      .pipe(
-        tap(_ => this.loading = true),
-        debounceTime(3000),
-        distinctUntilChanged(),
-        tap(_ => this.loading = false)
-      );
+  ngAfterViewInit () {
+    this.subscribeInputUsername()
   }
 
-  openModal(id: string): void {
-    this.modalService.open(id);
+  subscribeInputUsername () {
+    const terms$ = fromEvent<any>(
+      this.searchUsername.nativeElement,
+      'blur'
+    ).pipe(
+      map(event => event.target.value),
+      startWith(''),
+      debounceTime(400),
+      distinctUntilChanged()
+    )
+    this.usernameSubscription = terms$.subscribe(toSearch => {
+      this.users$.subscribe(data => {
+        const criteria: String = toSearch
+        const users: any = data.map(x => x.username.toLowerCase());
+        const exist: Boolean = users.includes(criteria.trim().toLowerCase());
+        console.log({ exist });
+        if (exist) {
+          this.userIdExist = true
+        } else this.userIdExist = false
+      })
+    })
   }
 
-  closeModal(id: string): void {
-    this.modalService.close(id);
+  refresh (): void {
+    this.users$ = this.userService.getUsers(this.token, this.filter).pipe(
+      tap(_ => (this.loading = true)),
+      debounceTime(3000),
+      distinctUntilChanged(),
+      tap(_ => (this.loading = false))
+    )
   }
 
-  submit(data: NgForm) {
-    console.log({ data });
+  openModal (id: string): void {
+    this.modalService.open(id)
   }
 
+  closeModal (id: string): void {
+    this.modalService.close(id)
+  }
+
+  submit (data: NgForm) {
+    console.log({ data })
+  }
 }
